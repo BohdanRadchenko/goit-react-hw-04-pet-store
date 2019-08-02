f96c90a4ee08d7120a74531f952134ef9c1b6a9f
BohdanRadchenko <radchenkobs@gmail.com>
2019-08-02 11:09:54 +0300

    Updates

---
+++ precache-manifest.380ec36ed13596dd091ef08b97dd1c80.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "631bf76604b5490b191633d5ea58d68b",
+    "url": "/goit-react-hw-04-pets-shop/index.html"
+  },
+  {
+    "revision": "e42045dee459d6a063e5",
+    "url": "/goit-react-hw-04-pets-shop/static/css/Pets-page.f1126fb1.chunk.css"
+  },
+  {
+    "revision": "822b9205de8c1b1a1709",
+    "url": "/goit-react-hw-04-pets-shop/static/css/main.b2521b04.chunk.css"
+  },
+  {
+    "revision": "fc73b80c1655a29c71bf",
+    "url": "/goit-react-hw-04-pets-shop/static/js/6.7f25b761.chunk.js"
+  },
+  {
+    "revision": "b9c483e947a7f1cb18a7",
+    "url": "/goit-react-hw-04-pets-shop/static/js/About-page.10f83e7c.chunk.js"
+  },
+  {
+    "revision": "06a80cf3fa1e4490dd63",
+    "url": "/goit-react-hw-04-pets-shop/static/js/Pet-page.7a1439c0.chunk.js"
+  },
+  {
+    "revision": "e42045dee459d6a063e5",
+    "url": "/goit-react-hw-04-pets-shop/static/js/Pets-page.722e2d84.chunk.js"
+  },
+  {
+    "revision": "a27cd037640281752285",
+    "url": "/goit-react-hw-04-pets-shop/static/js/home-page.7b3aa72c.chunk.js"
+  },
+  {
+    "revision": "822b9205de8c1b1a1709",
+    "url": "/goit-react-hw-04-pets-shop/static/js/main.96d8a3f0.chunk.js"
+  },
+  {
+    "revision": "c86aaffdb348071daae0",
+    "url": "/goit-react-hw-04-pets-shop/static/js/runtime~main.81f95045.js"
+  }
+]);
