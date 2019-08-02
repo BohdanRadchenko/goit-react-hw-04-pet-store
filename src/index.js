441cf210477f88b1d336bb2310e635f486e83568
BohdanRadchenko <radchenkobs@gmail.com>
2019-08-02 09:59:33 +0300

    create hw 04 pets test folder

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
+/*eslint-disable*/
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <Route component={App} />
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
